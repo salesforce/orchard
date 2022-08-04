8ed814ee21000d3abffad319daa194901662297c
Thomas Chang <thomaschang@salesforce.com>
2022-08-04 16:40:52 -0700

    Enable Retry default parameter configurable

--- orchard-core/src/main/scala/com/salesforce/mce/orchard/util/Retry.scala
+++ orchard-core/src/main/scala/com/salesforce/mce/orchard/util/Retry.scala
@@ -1,18 +1,18 @@
 package com.salesforce.mce.orchard.util
 
-import com.krux.stubborn.policy.ExponentialBackoff
+import com.krux.stubborn.policy.{ExponentialBackoff, Policy}
 
 import scala.util.{Failure, Try}
 
 object Retry {
-  private val MAX_RETRIES = 3
-  private val policy = ExponentialBackoff(100, 10000)
+  val maxRetries = 3
+  val policy: Policy = ExponentialBackoff(100, 10000)
 
   @annotation.tailrec
-  def apply[T](numTries: Int = MAX_RETRIES)(fn: => T): Try[T] = {
+  def apply[T](numTries: Int = maxRetries)(fn: => T): Try[T] = {
     Try { fn } match {
       case Failure(_) if numTries > 1 =>
-        val delay = policy.retryDelay(MAX_RETRIES - numTries)
+        val delay = policy.retryDelay(maxRetries - numTries)
         Thread.sleep(delay)
         Retry(numTries - 1)(fn)
       case fn => fn
