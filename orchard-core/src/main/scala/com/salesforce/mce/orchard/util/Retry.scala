ab696e4cc664120c11f50f54d3847f84a70bd3c4
Thomas Chang <thomaschang@salesforce.com>
2022-08-05 14:53:22 -0700

    Refactor Retry object default values to traits

--- orchard-core/src/main/scala/com/salesforce/mce/orchard/util/Retry.scala
+++ orchard-core/src/main/scala/com/salesforce/mce/orchard/util/Retry.scala
@@ -4,17 +4,21 @@ import scala.util.{Failure, Try}
 
 import com.krux.stubborn.policy.{ExponentialBackoff, Policy}
 
-object Retry {
-  val maxRetries = 3
-  val policy: Policy = ExponentialBackoff(100, 10000)
+trait RetryDefaults {
+  protected val defaultMaxRetries = 3
+  protected val defaultPolicy: Policy = ExponentialBackoff(100, 10000)
+}
 
+object Retry extends RetryDefaults {
   @annotation.tailrec
-  def apply[T](numTries: Int = maxRetries)(fn: => T): Try[T] = {
+  def apply[T](numTries: Int = defaultMaxRetries, policy: Policy = defaultPolicy)(
+    fn: => T
+  ): Try[T] = {
     Try { fn } match {
       case Failure(_) if numTries > 1 =>
-        val delay = policy.retryDelay(maxRetries - numTries)
+        val delay = policy.retryDelay(defaultMaxRetries - numTries)
         Thread.sleep(delay)
-        Retry(numTries - 1)(fn)
+        Retry(numTries - 1, policy)(fn)
       case fn => fn
     }
   }
