fdec24e6f38135638f5b873f643d604023f416ab
Thomas Chang <thomaschang@salesforce.com>
2022-08-08 08:50:22 -0700

    Incorporate stubborn library's default parameters

--- orchard-core/src/main/scala/com/salesforce/mce/orchard/util/Retry.scala
+++ orchard-core/src/main/scala/com/salesforce/mce/orchard/util/Retry.scala
@@ -1,22 +1,18 @@
 package com.salesforce.mce.orchard.util
 
+import com.krux.stubborn.RetryDefaults
+import com.krux.stubborn.policy.Policy
+
 import scala.util.{Failure, Try}
-
-import com.krux.stubborn.policy.{ExponentialBackoff, Policy}
-
-trait RetryDefaults {
-  protected val defaultMaxRetries = 3
-  protected val defaultPolicy: Policy = ExponentialBackoff(100, 10000)
-}
 
 object Retry extends RetryDefaults {
   @annotation.tailrec
-  def apply[T](numTries: Int = defaultMaxRetries, policy: Policy = defaultPolicy)(
+  def apply[T](numTries: Int = defaultMaxRetry, policy: Policy = defaultPolicy)(
     fn: => T
   ): Try[T] = {
     Try { fn } match {
       case Failure(_) if numTries > 1 =>
-        val delay = policy.retryDelay(defaultMaxRetries - numTries)
+        val delay = policy.retryDelay(defaultMaxRetry - numTries)
         Thread.sleep(delay)
         Retry(numTries - 1, policy)(fn)
       case fn => fn
