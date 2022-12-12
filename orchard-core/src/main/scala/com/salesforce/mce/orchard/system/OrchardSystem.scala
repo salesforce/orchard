08a7f3922e308a7142ee99986c75f245a29d6f25
Kexin Xie <realstraw@gmail.com>
2022-12-11 19:37:51 -0800

    wip: start implement the WorkflowSheduler

--- orchard-core/src/main/scala/com/salesforce/mce/orchard/system/OrchardSystem.scala
+++ orchard-core/src/main/scala/com/salesforce/mce/orchard/system/OrchardSystem.scala
@@ -9,6 +9,7 @@ package com.salesforce.mce.orchard.system
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
+
 import com.salesforce.mce.orchard.db.OrchardDatabase
 import com.salesforce.mce.orchard.system.actor.WorkflowMgr
 
@@ -17,8 +18,7 @@ object OrchardSystem {
   sealed trait Msg
   case class ActivateMsg(database: OrchardDatabase, workflowId: String) extends Msg
 
-  def apply(): Behavior[Msg] =
-    Behaviors.setup(context => new OrchardSystem(context))
+  def apply(): Behavior[Msg] = Behaviors.setup(context => new OrchardSystem(context))
 
 }
 
