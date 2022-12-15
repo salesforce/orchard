463eed3dfd2de5e0062725b6272253ce16556389
Annie Zhang <37345842+anniezhang77@users.noreply.github.com>
2022-12-14 17:52:06 -0800

    make output uri and timeout optional (#58)

--- orchard-provider-aws/src/main/scala/com/salesforce/mce/orchard/io/aws/activity/ShellCommandActivity.scala
+++ orchard-provider-aws/src/main/scala/com/salesforce/mce/orchard/io/aws/activity/ShellCommandActivity.scala
@@ -13,21 +13,21 @@ import java.time.temporal.ChronoUnit
 import scala.jdk.CollectionConverters.{MapHasAsJava, SeqHasAsJava}
 
 import org.slf4j.LoggerFactory
-import play.api.libs.json.{JsResult, JsValue, Json}
+import play.api.libs.json.{JsResult, JsValue, Json, Reads}
 import software.amazon.awssdk.services.ssm.model.SendCommandRequest
 
 import com.salesforce.mce.orchard.io.ActivityIO
-import com.salesforce.mce.orchard.io.aws.Client
+import com.salesforce.mce.orchard.io.aws.util.S3Uri
+import com.salesforce.mce.orchard.io.aws.{Client, ProviderSettings}
 import com.salesforce.mce.orchard.util.RetryHelper._
 
 case class ShellCommandActivity(
   name: String,
   lines: Seq[String],
-  outputS3BucketName: String,
-  outputS3KeyPrefix: String,
-  executionTimeout: Int,
-  deliveryTimeout: Int,
-  ec2InstanceId: String
+  ec2InstanceId: String,
+  outputUri: Option[String],
+  executionTimeout: Option[Int],
+  deliveryTimeout: Option[Int]
 ) extends Ec2Activity(name, ec2InstanceId) {
 
   private val logger = LoggerFactory.getLogger(getClass)
@@ -44,21 +44,27 @@ case class ShellCommandActivity(
 
     lazy val ts = LocalDateTime.now(ZoneOffset.UTC).truncatedTo(ChronoUnit.MINUTES)
     val client = Client.ssm()
-    val paraMap = Map(
-      "commands" -> lines.asJava,
-      "executionTimeout" -> List(executionTimeout.toString).asJava
-    )
-    val request =
-      SendCommandRequest
-        .builder()
-        .documentName(RunShellScriptDocument)
-        .instanceIds(ec2InstanceId)
-        .comment(getComment(getClass))
-        .parameters(paraMap.asJava)
-        .timeoutSeconds(deliveryTimeout)
-        .outputS3BucketName(outputS3BucketName)
-        .outputS3KeyPrefix(s"${outputS3KeyPrefix.stripSuffix("/")}/${name}_$ts")
-        .build()
+    val paraMap =
+      Map("commands" -> lines.asJava) ++
+      executionTimeout.map(v => "executionTimeout" -> List(v.toString).asJava).toMap
+    val loggingUri = outputUri.orElse(ProviderSettings().loggingUri).map(S3Uri.apply)
+
+    val builder = loggingUri
+      .foldLeft(
+        SendCommandRequest
+          .builder()
+          .documentName(RunShellScriptDocument)
+          .instanceIds(ec2InstanceId)
+          .comment(getComment(getClass))
+          .parameters(paraMap.asJava)
+      )((r, uri) =>
+        r
+          .outputS3BucketName(uri.bucket)
+          .outputS3KeyPrefix(s"${uri.path.stripSuffix("/")}/${name}_$ts")
+      )
+    val request = deliveryTimeout
+      .foldLeft(builder)(_.timeoutSeconds(_))
+      .build()
     val response = client.sendCommand(request)
     client.close()
     val command = response.command()
@@ -69,32 +75,35 @@ case class ShellCommandActivity(
   }
 
   override def toString: String = {
-    s"ShellCommandActivity: $name lines=$lines outputS3BucketName=$outputS3BucketName " +
-      s"outputS3KeyPrefix=$outputS3KeyPrefix executionTimeout=$executionTimeout deliveryTimeout=$deliveryTimeout.  " +
+    s"ShellCommandActivity: $name lines=$lines outputUri=$outputUri " +
+      s"executionTimeout=$executionTimeout deliveryTimeout=$deliveryTimeout.  " +
       s"${super.toString}"
   }
 }
 
 object ShellCommandActivity {
 
+  case class Spec(
+    lines: Seq[String],
+    outputUri: Option[String],
+    executionTimeout: Option[Int],
+    deliveryTimeout: Option[Int]
+  )
+
+  implicit val specReads: Reads[Spec] = Json.reads[Spec]
+
   def decode(conf: ActivityIO.Conf): JsResult[ShellCommandActivity] = {
     for {
-      lines <- (conf.activitySpec \ "lines").validate[Seq[String]]
-      outputS3BucketName <- (conf.activitySpec \ "outputS3BucketName").validate[String]
-      outputS3KeyPrefix <- (conf.activitySpec \ "outputS3KeyPrefix").validate[String]
-      executionTimeout <- (conf.activitySpec \ "executionTimeout").validate[Int]
-      deliveryTimeout <- (conf.activitySpec \ "deliveryTimeout").validate[Int]
+      spec <- conf.activitySpec.validate[Spec]
       ec2InstanceId <- (conf.resourceInstSpec \ "ec2InstanceId").validate[String]
-    } yield {
+    } yield
       ShellCommandActivity(
         s"${conf.workflowId}_act-${conf.activityId}_${conf.attemptId}",
-        lines,
-        outputS3BucketName,
-        outputS3KeyPrefix,
-        executionTimeout,
-        deliveryTimeout,
-        ec2InstanceId
+        spec.lines,
+        ec2InstanceId,
+        spec.outputUri,
+        spec.executionTimeout,
+        spec.deliveryTimeout
       )
-    }
   }
 }
