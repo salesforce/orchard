8364b9dac462bf89b2bb25f46f4535a3b5027b4a
simon.chow <simon.chow@salesforce.com>
2023-07-27 09:40:20 -0700

    OrchardSystem restart supervision using Configuration
    Signed-off-by: simon.chow <simon.chow@salesforce.com>

--- orchard-ws/app/services/OrchardSystemService.scala
+++ orchard-ws/app/services/OrchardSystemService.scala
@@ -10,40 +10,46 @@ package services
 import javax.inject._
 
 import scala.concurrent.duration._
-import scala.util.{Success, Try}
 
 import akka.actor.ActorSystem
 import akka.actor.typed.{ActorRef, Behavior, SupervisorStrategy}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.adapter._
-import com.typesafe.config.{Config, ConfigFactory}
+import play.api.Configuration
 
 import com.salesforce.mce.orchard.system.OrchardSystem
 
 @Singleton
-class OrchardSystemService @Inject() (actorSystem: ActorSystem, databaseService: DatabaseService) {
+class OrchardSystemService @Inject() (
+  actorSystem: ActorSystem,
+  databaseService: DatabaseService,
+  conf: Configuration
+) {
 
-  private val config: Config = ConfigFactory.load()
-  private val restartMaxNrOfRetries: Int = Try {
-    config.getInt("orchard.system.restart-num-of-retries")
-  } match {
-    case Success(x) => x
-    case _ => 10
-  }
-  private val restartTimeRange: FiniteDuration = Try {
-    config.getInt("orchard.system.restart-time-range")
-  } match {
-    case Success(x) => x.seconds
-    case _ => 60.seconds
-  }
+  val numRetriesAndTimeRange = for {
+    n <- conf.getOptional[Int]("orchard.system.restart-num-of-retries")
+    t <- conf.getOptional[Long]("orchard.system.restart-time-range")
+  } yield (n, t)
 
   private val supervisedOrchardSystem: Behavior[OrchardSystem.Msg] =
-    Behaviors
-      .supervise(OrchardSystem.apply(databaseService.orchardDB))
-      .onFailure(
-        SupervisorStrategy.restart
-          .withLimit(maxNrOfRetries = restartMaxNrOfRetries, withinTimeRange = restartTimeRange)
-      )
+    if (numRetriesAndTimeRange.isEmpty) {
+      Behaviors
+        .supervise(OrchardSystem.apply(databaseService.orchardDB))
+        .onFailure(
+          SupervisorStrategy.restart
+        )
+    } else {
+      val (restartMaxNrOfRetries, restartTimeRange) = numRetriesAndTimeRange.get
+      Behaviors
+        .supervise(OrchardSystem.apply(databaseService.orchardDB))
+        .onFailure(
+          SupervisorStrategy.restart
+            .withLimit(
+              maxNrOfRetries = restartMaxNrOfRetries,
+              withinTimeRange = restartTimeRange.seconds
+            )
+        )
+    }
 
   val orchard: ActorRef[OrchardSystem.Msg] =
     actorSystem.spawn(supervisedOrchardSystem, "orchard-system")
