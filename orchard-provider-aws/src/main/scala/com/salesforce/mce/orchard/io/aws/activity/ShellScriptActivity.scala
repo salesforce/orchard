e645e5653e892fadb106724b923e9b85c587e9cd
crm-dhu <donglin.hu@salesforce.com>
2024-11-20 15:48:04 -0800

    fix missing endpoint and region in decoding (#135)
    * fix missing endpoint and region in decoding
    
    * remove default values
    
    * remove default values
    
    * version bump

--- orchard-provider-aws/src/main/scala/com/salesforce/mce/orchard/io/aws/activity/ShellScriptActivity.scala
+++ orchard-provider-aws/src/main/scala/com/salesforce/mce/orchard/io/aws/activity/ShellScriptActivity.scala
@@ -29,8 +29,8 @@ case class ShellScriptActivity(
   outputUri: Option[String],
   executionTimeout: Option[Int],
   deliveryTimeout: Option[Int],
-  region: Option[String] = None,
-  endPoint: Option[String] = None
+  region: Option[String],
+  endPoint: Option[String]
 ) extends Ec2Activity(name, ec2InstanceId) {
 
   private val logger = LoggerFactory.getLogger(getClass)
@@ -111,7 +111,9 @@ object ShellScriptActivity {
     args: Seq[String],
     outputUri: Option[String],
     executionTimeout: Option[Int],
-    deliveryTimeout: Option[Int]
+    deliveryTimeout: Option[Int],
+    region: Option[String],
+    endPoint: Option[String]
   )
 
   implicit val specReads: Reads[Spec] = Json.reads[Spec]
@@ -128,7 +130,9 @@ object ShellScriptActivity {
         ec2InstanceId,
         spec.outputUri,
         spec.executionTimeout,
-        spec.deliveryTimeout
+        spec.deliveryTimeout,
+        spec.region,
+        spec.endPoint
       )
   }
 
--- version.sbt
+++ version.sbt
@@ -1,1 +1,1 @@
-ThisBuild / version := "0.23.5"
+ThisBuild / version := "0.23.6"
